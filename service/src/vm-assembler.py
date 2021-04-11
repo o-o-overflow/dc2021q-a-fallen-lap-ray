0d7e2c8fcf1e6b4fe8653170b02b29cea4e7c403
Adam Doupe <adamdoupe@gmail.com>
2021-04-11 10:03:48 -0700

    Added a .data segment to the vm assembler to load data into the binary that we can use.

--- service/src/vm-assembler.py
+++ service/src/vm-assembler.py
@@ -44,12 +44,16 @@ REGISTERS = { r.name: r for r in _REGISTER_LIST }
 OPCODES = { o.name: o for o in _OPCODE_LIST }
 SYSCALLS = { s.name: s for s in _SYSCALL_LIST }
 
-def parse_arg(raw_arg):
+INSTRUCTION_SIZE = 3
+
+def parse_arg(raw_arg, labels, data_mapping, error_on_fail=True):
     if raw_arg in REGISTERS:
         return REGISTERS[raw_arg]
     elif raw_arg in SYSCALLS:
         return SYSCALLS[raw_arg]
-
+    elif raw_arg in data_mapping:
+        val = data_mapping[raw_arg]
+        return Literal(str(val), val)
     try:
         val = int(raw_arg, base=10)
         return Literal(str(val), val)
@@ -62,14 +66,22 @@ def parse_arg(raw_arg):
             return Literal(hex(val), val)
         except ValueError:
             pass
-    l.error(f"I don't know how to parse {raw_arg=}")
-    sys.exit(-1)
-    return -1
 
-def parse_into_instructions(input):
+    if raw_arg in labels:
+        dest = labels[raw_arg] * INSTRUCTION_SIZE
+        return Literal(str(dest), dest)
+
+    if error_on_fail:
+        l.error(f"I don't know how to parse {raw_arg=}")
+        sys.exit(-1)
+        return -1
+    else:
+        return None
+
+def parse_into_instructions(lines, labels, data_mapping, data):
     instructions = []
     i = 0
-    for line in input:
+    for line in lines:
         i += 1
         line = line.strip()
         l.debug(f"Analyzing line {i}")
@@ -79,7 +91,8 @@ def parse_into_instructions(input):
         args = line.split()
         l.debug(f"{args=}")
         if len(args) == 1:
-            l.error("TODO lables")
+            assert(False)
+
         elif len(args) != 3:
             l.error(f"Don't know how to process a line with {len(args)} arguments on line {i}.")
             sys.exit(-1)
@@ -96,8 +109,8 @@ def parse_into_instructions(input):
                     sys.exit(-10)
 
                 inst_opcodes.append(OPCODES[op])
-            inst_arg1 = parse_arg(arg1)
-            inst_arg2 = parse_arg(arg2)
+            inst_arg1 = parse_arg(arg1, labels, data_mapping)
+            inst_arg2 = parse_arg(arg2, labels, data_mapping)
 
             l.debug(f"instruction Line {i} {inst_opcodes=} {inst_arg1=} {inst_arg2=}")
 
@@ -106,7 +119,8 @@ def parse_into_instructions(input):
     # TODO: resolve lables
     return instructions
 
-def output_instructions(instructions, output_file):
+def output_instructions(instructions, data, output_file):
+    size_inst = 0
     with open(output_file, 'wb') as output:
         for inst in instructions:
             opcode_value = 0
@@ -117,14 +131,101 @@ def output_instructions(instructions, output_file):
                                             opcode_value,
                                             inst.arg1.spec,
                                             inst.arg2.spec)
+            size_inst += len(instruction_value)
             output.write(instruction_value)
+        output.write(data)
+    l.info(f"Program output to {output_file} has {size_inst} bytes code and {len(data)} bytes data total {size_inst+len(data)}")
+
+def parse_labels(input):
+    filtered_lines = []
+    labels = {}
+    i = 0
+    for line in input:
+        i += 1
+        line = line.strip()
+        args = line.split()
+
+        if (not line) or line.startswith("#"):
+            continue
+
+        if len(args) == 1:
+            the_label = args[0]
+            if the_label == ".data":
+                filtered_lines.append(line)
+                continue
+
+            if not the_label.endswith(':'):
+                l.error(f"Label on line {i} does not end with a colon")
+                sys.exit(-1)
+            label = the_label.rstrip(':')
+
+            if label in labels:
+                l.error(f"Label on line {i} is {label}, however it's already defined")
+                sys.exit(-1)
+
+            if parse_arg(label, labels, {}, error_on_fail=False) is not None:
+                l.error(f"Label on line {i} is {label}, but that's already a known")
+                sys.exit(-1)
+
+            labels[label] = len(filtered_lines)
+
+            l.debug(f"Found {label=} {labels[label]=}")
+        else:
+            filtered_lines.append(line)
+    return labels, filtered_lines
+
+def parse_data_segment(lines):
+    filtered_lines = []
+    data_mapping = {}
+    data_values = {}
+    data = b""
+    in_data_segment = False
+    data_offset = None
+    for line in lines:
+        line = line.strip()
+        if line == ".data":
+            in_data_segment = True
+            data_offset = len(filtered_lines) * INSTRUCTION_SIZE
+        elif in_data_segment:
+            args = line.split()
+            if len(args) != 2:
+                l.error(f"Malformed {args} in the .data segment")
+                sys.exit(-1)
+            name = args[0]
+            if name in data_mapping:
+                l.error(f"Error in .data segment, {name} already defined")
+                sys.exit(-1)
+            raw_value = args[1]
+            value = eval(raw_value, data_values)
+            data_values[name] = value
+
+            if isinstance(value, int):
+                data_mapping[name] = data_offset
+                stored_data = struct.pack('<q', value)
+                data += stored_data
+                data_offset += len(stored_data)
+            elif isinstance(value, str):
+                data_mapping[name] = data_offset
+                stored_data = value.encode()
+                data += stored_data
+                data_offset += len(stored_data)
+            else:
+                l.error(f"Don't know how to handle the .data segment {name} {type(value)}")
+                sys.exit(-1)
+
+        else:
+            filtered_lines.append(line)
+
+    return data_mapping, data, filtered_lines
 
 def main(input_file, output_file):
 
     with open(input_file, 'r') as input:
-        instructions = parse_into_instructions(input)
+        labels, lines = parse_labels(input)
+        data_mapping, data, lines = parse_data_segment(lines)
+        instructions = parse_into_instructions(lines, labels, data_mapping, data)
 
-    output_instructions(instructions, output_file)
+    output_instructions(instructions, data, output_file)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog="vm-assembler")
